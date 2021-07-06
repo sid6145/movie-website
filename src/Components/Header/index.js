6986ae64c1eb05c0a3e2c8dd9a2fcc4a9974600e
sid6145 <sidtdesh@gmail.com>
2021-07-06 12:07:55 +0530

    liked navbar links to their respective pages and made search bar responsive

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,19 +2,32 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
 
 function Header(props) {
   return (
-    <Navbar className="px-5" collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand  href="#home">
-        IMDB
-      </Navbar.Brand>
+    <Navbar
+      className="px-5"
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
+      <Link className="logo" to="/">
+        <Navbar.Brand href="#home">IMDB</Navbar.Brand>
+      </Link>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ms-auto">
-          <Nav.Link href="#features">Home</Nav.Link>
-          <Nav.Link href="#pricing">TopRated</Nav.Link>
-          <Nav.Link href="#pricing">Watchlist</Nav.Link>
+          <Link className="link" to="/">
+            Home
+          </Link>
+          <Link className="link" to="/toprated">
+            TopRated
+          </Link>
+          <Link className="link" to="/watchlist">
+            Watchlist
+          </Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
